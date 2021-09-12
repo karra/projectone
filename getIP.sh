6e14aa8787d6bdf25cfee66fbcae8134709680ad
Srikanth Karra <karra.srikanth@gmail.com>
2021-09-12 11:41:04 +0530

    Updating this branch, removing ansible stuff

--- getIP.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null
-ip=`az vm list-ip-addresses -n newVM -g newRG --query [].virtualMachine.network.publicIpAddresses[0].ipAddress -o tsv`
-echo -e "[azurevm]\n$ip" | sudo tee -a /etc/ansible/hosts > /dev/null

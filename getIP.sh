cb817e836adf2fc4ce67a0765e2166b591beb959
Srikanth Karra <srikanth.karra@techmahindra.com>
2021-09-12 05:19:59 +0000

    Updated getIP & playbook2

--- getIP.sh
+++ getIP.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo sed -i '/azurevm/d' /etc/ansible/hosts
 az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null
 ip=`az vm list-ip-addresses -n newVM -g newRG --query [].virtualMachine.network.publicIpAddresses[0].ipAddress -o tsv`
-echo -e "[azurevm]\n$ip" | sudo tee -a /etc/ansible/hosts > /dev/null
+echo -e "azurevm ansible_host=$ip" | sudo tee -a /etc/ansible/hosts > /dev/null
